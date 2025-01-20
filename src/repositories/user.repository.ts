19833d3355d850366c77b51c6b6b5de6d09beecc
Quentin Stempfer <quentin.stempfer@supdevinci-edu.fr>
2025-01-20 15:00:22 +0100

    sql => mongo

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,45 +1,12 @@
-import { UserEntity } from "../databases/mysql/user.entity";
-import { Repository } from "typeorm";
-import { connectMySQLDB } from "../configs/databases/mysql.config";
-import { UserToCreateDTO } from "../types/user/dtos";
-import { userToCreateInput } from "../types/user/Inputs";
+import { IUser, User } from "../databases/mongodb/user.model";
 
-export class UserRepository{
+export class UserRepository {
 
-    private userDB: Repository<UserEntity>;
-
-    constructor() {
-        this.userDB = connectMySQLDB.getRepository(UserEntity);
+    createUser(user: IUser): IUser {
+        return new User(user);
     }
 
-    createUser(user: userToCreateInput): UserEntity {
-        const newUser = this.userDB.create(user);
-        return newUser
-    }
-
-    async saveUser(user: UserEntity): Promise<UserEntity> {
-        return this.userDB.save(user);
-    }
-
-    async findUserById(userId: number): Promise<UserEntity | null> {
-        return await this.userDB.findOneBy({ id: userId });
-    }
-
-    async findUserByEmail(email: string): Promise<UserEntity | null> {
-        return await this.userDB.findOneBy({ email: email });
-    }
-
-    async findAllUsers(): Promise<UserEntity[]> {
-        return await this.userDB.find();
-    }
-
-    async updateUser(userId: number, user: UserToCreateDTO): Promise<UserEntity | null> {
-        await this.userDB.update(userId, user);
-        return await this.findUserById(userId);
-    }
-
-    async deleteUser(userId: number): Promise<UserEntity | null> {
-        await this.userDB.delete(userId);
-        return await this.findUserById(userId);
+    async saveUser(user: IUser): Promise<IUser> {
+        return await (user as User).save();
     }
 }
