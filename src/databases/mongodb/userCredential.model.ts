34465a32a558c2f115cde5e7b8a8274770b363eb
Quentin Stempfer <quentin.stempfer@supdevinci-edu.fr>
2025-01-20 15:29:56 +0100

    password_hash

--- src/databases/mongodb/userCredential.model.ts
+++ src/databases/mongodb/userCredential.model.ts
@@ -3,12 +3,12 @@ import mongoose, { Schema } from "mongoose";
 export interface IUserCredential{
     _id: string;
     userId: string;
-    password: string;
+    password_hash: string;
 }
 
 const userCredentialSchema = new Schema({
     userId: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password_hash: { type: String, required: true },
 });
 
 export const UserCredentialModel = mongoose.model<IUserCredential>("UserCredential", userCredentialSchema);
