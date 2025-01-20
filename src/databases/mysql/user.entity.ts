db43630d28f059f56dbb2420f8826687de2589c3
Quentin Stempfer <quentin.stempfer@supdevinci-edu.fr>
2025-01-20 13:20:51 +0100

    test problème connexion bdd

--- src/databases/mysql/user.entity.ts
+++ src/databases/mysql/user.entity.ts
@@ -1,26 +1,20 @@
-import { Expose } from "class-transformer";
-import { Entity, PrimaryGeneratedColumn, Column, EntitySchema } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
 
 @Entity("users")
 export class UserEntity {
-  @Expose()
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Expose()
   @Column({ length: 50 })
   firstname: string;
 
-  @Expose()
   @Column({ length: 50 })
   lastname: string;
 
-  @Expose()
   @Column({ unique: true })
   email: string;
 
-  @Expose()
   @Column({ default: true })
   isActive: boolean;
 }
