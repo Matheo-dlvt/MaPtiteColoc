68f084fc4a85dc1e6b1ee40f5dafc9a9a75f49cf
Quentin Stempfer <quentin.stempfer@supdevinci-edu.fr>
2025-01-21 10:04:59 +0100

    update colocation dtos & service

--- src/services/colocation.service.ts
+++ src/services/colocation.service.ts
@@ -1,3 +1,4 @@
+import { ColocationModel } from "../databases/mongodb/colocation.model";
 import { ColocationRepository } from "../repositories/colocation.repository";
 import { ColocationToCreateDTO, ColocationToUpdateDTO, ColocationToSaveDTO } from "../types/colocation/dtos";
 import { ColocationPresenter } from "../types/colocation/presenters";
@@ -6,26 +7,34 @@ export class ColocationService {
   private colocationRepository: ColocationRepository = new ColocationRepository();
 
   async createColocation(colocationToCreate: ColocationToCreateDTO): Promise<ColocationPresenter> {
-    return await this.colocationRepository.createColocation(colocationToCreate);
+    const exitColoc = await this.colocationRepository.findColocationByName(colocationToCreate.name);
+    if (exitColoc) {
+      throw new Error("Colocation already exists");
+    }
+
+    const newColocation = new ColocationModel({
+      name: colocationToCreate.name,
+      adress: colocationToCreate.adress,
+      surface: colocationToCreate.surface,
+      rent: colocationToCreate.rent
+    });
+
+    return await this.colocationRepository.saveColocation(newColocation);
   }
 
-  async saveColocation(colocationToSave: ColocationToSaveDTO): Promise<ColocationPresenter> {
-    return await this.colocationRepository.saveColocation(colocationToSave);
-  }
-
-  async findColocationById(colocationId: string): Promise<ColocationPresenter> {
+  async findColocationById(colocationId: string): Promise<ColocationPresenter | null> {
     return await this.colocationRepository.findColocationById(colocationId);
   }
 
-  async findColocationByName(name: string): Promise<ColocationPresenter> {
+  async findColocationByName(name: string): Promise<ColocationPresenter | null> {
     return await this.colocationRepository.findColocationByName(name);
   }
 
-  async updateColocation(colocationId: string, colocationToUpdate: ColocationToUpdateDTO): Promise<ColocationPresenter> {
-    return await this.colocationRepository.updateColocation(colocationId, colocationToUpdate);
-  }
+  // async updateColocation(colocationId: string, colocationToUpdate: ColocationToUpdateDTO): Promise<ColocationPresenter | null> {
+  //   return await this.colocationRepository.updateColocation(colocationId, colocationToUpdate);
+  // }
 
-  async deleteColocation(colocationId: string): Promise<ColocationPresenter> {
+  async deleteColocation(colocationId: string): Promise<ColocationPresenter | null> {
     return await this.colocationRepository.deleteColocation(colocationId);
   }
 }
