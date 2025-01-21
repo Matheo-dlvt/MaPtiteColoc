a856291c0a46d7f882430a37492bc558356b39b8
Mathéo Delvert <115637444+Matheo-dlvt@users.noreply.github.com>
2025-01-21 11:02:34 +0100

    update auth.middleware.ts

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,17 +1,17 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { UserRepository } from "../repositories/user.repository";
-import { UserCredentialRepository } from '../repositories/userCredential.repository';
-import { ErrorResponse } from "../utils/error.utils";
+import { CustomError } from '../utils/customError.exception';
+import { HTTPStatusCode } from '../types/errors';
+
 
 const userRepository = new UserRepository();
-const userCredentialRepository = new UserCredentialRepository();
 
 export const authenticateJWT = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
     const token = req.header('Authorization')?.replace('Bearer ', '');
 
     if (!token) {
-        return res.status(401).json(new ErrorResponse(401, "AUTH_ERROR", "No token provided"));
+        return res.status(401).json(new CustomError("No token provided", "ntp001", HTTPStatusCode.UNAUTHORIZED));
     }
 
     try {
@@ -19,17 +19,17 @@ export const authenticateJWT = async (req: Request, res: Response, next: NextFun
         const user = await userRepository.findUserById(decoded.userId);
     
         if (!user) {
-            return res.status(401).json(new ErrorResponse(401, "USER_NOT_FOUND", "User not found"))
+            return res.status(401).json(new CustomError("User not found", "unf002", HTTPStatusCode.BAD_REQUEST))
         }
         const userCredential = await userCredentialRepository.findUserCredentialByUserId(decoded.userId);
         if (!userCredential) {
-            return res.status(401).json(new ErrorResponse(401, "USER_NOT_FOUND", "User not found"))
+            return res.status(401).json(new CustomError("User not found", "unf003", HTTPStatusCode.BAD_REQUEST))
         }
-        const { password_hash, ...userWithoutPassword } = user;
+        const { password, ...userWithoutPassword } = user;
         (req as any).user = userWithoutPassword; 
         next();
 
     } catch (error) {
-        return res.status(401).json(new ErrorResponse(401, "INVALID_TOKEN", "Invalid or expired token"))
+        return res.status(401).json(new CustomError("User not found", "unf004", HTTPStatusCode.BAD_REQUEST))
     }
 };
